bd8bd547696b0e124239a6686de7bdc5afcd6b6c
tibbittschris <christophertibbitts@Christophers-MacBook-Pro.local>
2015-11-05 14:38:14 -0700

    Redirect to login
    If not logged in, but trying to go to dashboard, it will redirect to
    the login page.

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,26 @@
 var router = express.Router();
 
 router.get('/dashboard', function(req, res){
-  var options = {
-    url: 'https://api.instagram.com/v1/users/self/feed?access_token=' + ACCESS_TOKEN
+  if (ACCESS_TOKEN === ''){
+    res.redirect('/')
   }
 
-  request.get(options, function(error, response, body){
-    var feed = JSON.parse(body)
-    //console.log(body)
-    res.render('dashboard', {
-       feed: feed.data,
-       layout: 'auth_base',
-       title: 'User Dashboard!',
-       pageintro: 'Welcome to your dashboard!'
-     })
-  })
+  else{
+    var options = {
+      url: 'https://api.instagram.com/v1/users/self/feed?access_token=' + ACCESS_TOKEN
+    }
+
+    request.get(options, function(error, response, body){
+      var feed = JSON.parse(body)
+      //console.log(body)
+      res.render('dashboard', {
+         feed: feed.data,
+         layout: 'auth_base',
+         title: 'User Dashboard!',
+         pageintro: 'Welcome to your dashboard!'
+       })
+    })
+  }
 })
 
 module.exports = router
